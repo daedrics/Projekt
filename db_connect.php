d10017c1cee671c9540294651986f6bbb3968a8e
Redion Muraj <murajr@yahoo.com>
2017-04-16 12:42:21 +0200

    first commit

---
+++ db_connect.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 4/16/2017
+ * Time: 11:38 AM
+ */
+
+
+$db_hostname = 'localhost';
+$db_database = 'CRM';
+$db_username = 'root';
+$db_password = '';
+
+$link = mysqli_connect($db_hostname,$db_username , $db_password, $db_database);
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+
+
+
+
+?>
---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+session_start();
+?>
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 4/16/2017
+ * Time: 11:45 AM
+ */
+
+include ("db_connect.php");
+
+if(isset($_POST['login_submit'])) {
+    $usrn = $_POST['username'];
+    $pass = $_POST["password"];
+    $query = mysqli_query($link, "SELECT * FROM `admin` WHERE `username` = '$usrn' AND `password` = '$pass';");
+    $rows=mysqli_num_rows($query);
+    if($rows!=0){
+        $r=mysqli_fetch_row($query);
+        $pid=$r[0];
+        $_SESSION['pid'] = $pid;
+        header("Location: crm_index.html");
+    }
+
+    else {
+        echo '<script language="javascript">';
+        echo 'alert("Te dhenat nuk jane te sakta!")';
+        echo '</script>';
+        echo "<script type='text/javascript'>location.href = 'login.html';</script>";
+
+    }
+}
