2d9ca2cf2eb761f73f018b9604500de17c03273f
Redion Muraj <murajr@yahoo.com>
2017-04-17 22:22:11 +0200

    a

--- User/arkiv.php
+++ User/arkiv.php
@@ -59,7 +59,7 @@ else{
                             <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
                             <li><a href="#"><i class="fa fa-gear"></i> Settings</a></li>
                             <li class="divider"></li>
-                            <li><a href="#"><i class="fa fa-power-off"></i> Log Out</a></li>
+                            <li><a href="../logout.php"><i class="fa fa-power-off"></i> Log Out</a></li>
 
                         </ul>
                     </li>
