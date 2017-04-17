4f63f184c682b4c4225be231f9ca78b548e6e7da
Redion Muraj <murajr@yahoo.com>
2017-04-17 18:39:27 +0200

    a

--- User/getdata.php
+++ User/getdata.php
@@ -2,7 +2,16 @@
 
 <?php
 include("../db_connect.php");
-$query = mysqli_query($link,"SELECT * FROM `operator` WHERE `Id_Student` = '$pid';");
+$query = mysqli_query($link,"SELECT * FROM `operator` WHERE `id` = '$pid';");
+$row=mysqli_fetch_row($query);
+$id=$row[0];
+$emer=$row[1];
+$mbiemer=$row[2];
+$username=$row[3];
+$pass=$row[4];
+
+
+
 
 
 
