aad759826515adb1eee08e3eb9e60a33823ce972
Redion Muraj <murajr@yahoo.com>
2017-04-17 22:25:08 +0200

    a

---
+++ Admin/getdata.php
@@ -0,0 +1,19 @@
+
+
+<?php
+include("../db_connect.php");
+$query = mysqli_query($link,"SELECT * FROM `admin` WHERE `id` = '$pid';");
+$row=mysqli_fetch_row($query);
+$id=$row[0];
+$emer=$row[1];
+$mbiemer=$row[2];
+$username=$row[3];
+$pass=$row[4];
+
+
+
+
+
+
+
+?>
