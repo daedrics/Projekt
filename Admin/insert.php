43f75319c0ed7734b1b1619aa1e57210d435aaca
Troller1996 <klevispashollari@rocketmail.com>
2017-04-20 22:18:51 +0200

    2

--- Admin/insert.php
+++ Admin/insert.php
@@ -74,7 +74,131 @@ else{
         </nav>
 		
 		
-		
+		<div id="page-wrapper">
+            <h3 class="text-muted" style="padding-bottom: 20px">Raccolta Dati (aggiungi contratto)</h3>
+            <form class="text-primary" >
+                <div class="row">
+                    <div class="form-group col-lg-1">
+                        <label >Operatori</label>
+                        <input type="text" class="form-control" name="operatori">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Gestore Telefonico</label>
+                        <input type="text" class="form-control" name="gestore">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label for="operatori">Tipologia CNT</label>
+                        <select  class="form-control" name="tipologia">
+                            <option value="" disabled selected>Scegli Tipologia Contratto</option>
+                            <option value="1">NIP</option>
+                            <option value="2">ULL</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label for="operatori">APP/CNT</label>
+                        <select  class="form-control" name="app">
+                            <option value="" disabled selected>Scegli APP/CNT Contratto</option>
+                            <option value="1">APP</option>
+                            <option value="2">CNT</option>
+                        </select>
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Numero Fisso</label>
+                        <input type="text" class="form-control" name="nr_fisso">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-2">
+                        <label>Comune</label>
+                        <input type="text" class="form-control" name="comune">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Provincia</label>
+                        <input type="text" class="form-control" name="provincia">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Cap</label>
+                        <input type="text" class="form-control" name="cap">
+                    </div>
+                    <div class="form-group col-lg-3">
+                        <label>Via</label>
+                        <input type="text" class="form-control" name="via">
+                    </div>
+                    <div class="form-group col-lg-1">
+                        <label>Nr Civico</label>
+                        <input type="text" class="form-control" name="nr_civico">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-3">
+                        <label>Nome</label>
+                        <input type="text" class="form-control" name="nome">
+                    </div>
+                    <div class="form-group col-lg-3">
+                        <label>Cognome</label>
+                        <input type="text" class="form-control" name="cognome">
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <label>Luogo di nascita</label>
+                        <input type="text" class="form-control" name="luogo_nasc">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-2">
+                        <label>N. Documento</label>
+                        <input type="text" class="form-control" name="n_doc">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Comune di Emessione</label>
+                        <input type="text" class="form-control" name="comune_emes">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Data di Rilascio</label>
+                        <input type="text" class="form-control" name="data_rilasc">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Data di Scadenza</label>
+                        <input type="text" class="form-control" name="data_scad">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Codice Fiscale</label>
+                        <input type="text" class="form-control" name="cod_fisc">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-lg-4">
+                        <label>Codice di Migrazione</label>
+                        <input type="text" class="form-control" name="cod_migr">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Recapito di Cell</label>
+                        <input type="text" class="form-control" name="rec_cell">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label>Operatore Cell</label>
+                        <input type="text" class="form-control" name="op_cell">
+                    </div>
+                    <div class="form-group col-lg-2">
+                        <label >Offerta Scelta</label>
+                        <select  class="form-control" name="offer_scelta">
+                            <option value="" disabled selected>Scegli Offerta</option>
+                            <option value="tutto voce">TUTTO VOCE</option>
+                            <option value="isl">ISL</option>
+                            <option value="tse">TSE</option>
+                            <option value="2">ISL</option>
+                            <option value="2">ISL</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="checkbox row">
+                    <label><input type="checkbox"> Remember me</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+
+
+            </form>
+        </div>
+
 
         
     </div>
