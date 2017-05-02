7c33c4a6089d1d29b8493ff1fee3fc78f4012de5
Troller1996 <klevispashollari@rocketmail.com>
2017-05-02 22:39:25 +0200

    123

--- Admin/excel.php
+++ Admin/excel.php
@@ -18,11 +18,36 @@ if(isset($_POST["export_excel"]))
 		<table class="table" bordered="1">
 		<tr>
 		<th> Id</th>
-		<th> emri </th>
-		<th> mbiemri </th>
-		<th> data </th>
-		<th> status </th>
-		<th> id operatori </th>';
+		<th> Nome </th>
+		<th> Cognome </th>
+		<th> Data </th>
+		<th> Stato </th>
+		<th> Gestore Telefonico</th>
+		<th> Tipologia CNT </th>
+		<th> APP/CNT </th>
+		<th> Numero Fisso </th>
+		<th> Comune </th>
+		<th> Provincia</th>
+		<th> Frazione </th>
+		<th> Cap </th>
+		<th> Via </th>
+		<th> Nr Civico </th>
+		<th>  Luogo di nascita</th>
+		<th> N. Documento </th>
+		<th> Comune di Emessione </th>
+		<th> Data di Rilascio </th>
+		<th> Data di Scadenza </th>
+		<th> Codice Fiscale</th>
+		<th> Codice di Migrazione</th>
+		<th> Recapito di Cell</th>
+		<th> Operatore Cell</th>
+		<th> Offerta Scelta</th>
+		<th> Cell Off TSM</th>
+		<th> ICCID (TSM)</th>
+		<th> Codice Op</th>
+		<th> Motivazione</th>
+		<th> Note</th>
+		<th> Id operatore </th>';
             while ($row = mysqli_fetch_array($result)) {
                 $output .= '
 			<tr>
@@ -31,7 +56,33 @@ if(isset($_POST["export_excel"]))
 			<td> ' . $row["mbiemer"] . '</td>
 			<td> ' . $row["data"] . '</td>
 			<td> ' . $row["status"] . '</td>
+			<td> ' . $row["gestore_tel"] . '</td>
+			<td> ' . $row["tipologia_cnt"] . '</td>
+			<td> ' . $row["app_cnt"] . '</td>
+			<td> ' . $row["numero_fisso"] . '</td>
+			<td> ' . $row["comune"] . '</td>
+			<td> ' . $row["provincia"] . '</td>
+			<td> ' . $row["frazione"] . '</td>
+			<td> ' . $row["cap"] . '</td>
+			<td> ' . $row["via"] . '</td>
+			<td> ' . $row["nr_civico"] . '</td>
+			<td> ' . $row["luogo_di_nastica"] . '</td>
+			<td> ' . $row["nr_documento"] . '</td>
+			<td> ' . $row["comune_emmissione"] . '</td>
+			<td> ' . $row["data_rilascio"] . '</td>
+			<td> ' . $row["data_scadenza"] . '</td>
+			<td> ' . $row["codice_fiscale"] . '</td>
+			<td> ' . $row["codice_migrazione"] . '</td>
+			<td> ' . $row["recapito_cell"] . '</td>
+			<td> ' . $row["operatore_cell"] . '</td>
+			<td> ' . $row["offerta_scelta"] . '</td>
+			<td> ' . $row["cell_off_tsm"] . '</td>
+			<td> ' . $row["iccid"] . '</td>
+			<td> ' . $row["codice_op"] . '</td>
+			<td> ' . $row["motivacione"] . '</td>
+			<td> ' . $row["note"] . '</td>
 			<td> ' . $row["#id_Operator"] . '</td>
+			
 			</tr> ';
 
             }
